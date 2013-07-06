a66678e978b90dda3dbd65ebed44333c4c40e593
GustavoFernandez <gustavo.27.92@hotmail.com>
2013-07-06 00:09:45 -0500

    remove

--- class/conexion.class.php
+++ class/conexion.class.php
@@ -2,7 +2,7 @@
 class Conexion{
 
 	public static function get_conexion(){
-		$con = new mysqli("localhost", "root", "123456", "sistema-evaluacion");
+		$con = new mysqli("localhost", "root", "", "sistema-evaluacion");
 		$con->query("SET NAMES 'utf8'");
 		return $con;
 	}
